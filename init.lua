feaab3200ab000ebdfbf22033bc691867bbcad88
Armand Delcros <armand.delcros@inha.fr>
2025-04-15 22:08:53 +0200

    add keyboard shortcut to kill Buffer  to <leader>D

--- init.lua
+++ init.lua
@@ -171,6 +171,9 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
+-- Kill buffer keymap
+vim.keymap.set('n', '<leader>D', '<cmd>Bdelete<cr>', { desc = 'Kill Buffer' })
+
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
